b236589a8ff88e13f898372fde57a8165b2aae7d
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-04-19 21:35:38 +0200

    Add files

---
+++ CheckModuleData.ps1
@@ -0,0 +1,84 @@
+<#
+  .SYNOPSIS
+  Analyses existing cmdlets in Teams modules
+  
+  .DESCRIPTION
+  Version: 0.1
+  Analyses existing cmdlets in Teams modules
+  and detects any new additions
+
+  .NOTES
+  Author: Robert Dyjas https://dyjas.cc
+
+  Workflow:
+  - 
+
+  Standards:
+  - 
+
+  Features planned:
+  - Documentation
+
+  Known issues:
+  -
+
+  .EXAMPLE
+  CheckModuleData.ps1
+#>
+
+# ================
+#region Variables
+# ================
+
+# ================
+#endregion Variables
+# ================
+
+# ================
+#region Processing
+# ================
+
+# Install newest module
+Install-Module MicrosoftTeams -Scope CurrentUser -AllowPrerelease
+
+Get-Module MicrosoftTeams
+
+# Connect
+
+# ================
+#region Process cmdlets
+# ================
+
+# ================
+#endregion Process cmdlets
+# ================
+
+# ================
+#region Process cmdlet params
+# ================
+
+# ================
+#endregion Process cmdlet params
+# ================
+
+# ================
+#region Process policies
+# ================
+
+# ================
+#endregion Process policies
+# ================
+
+# Disconnect Teams
+
+# ================
+#endregion Processing
+# ================
+
+# ================
+#region Cleanup
+# ================
+
+# ================
+#endregion Cleanup
+# ================
