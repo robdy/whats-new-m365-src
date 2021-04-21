23df0fd233095df88421b1cea8c93f2e7bb66692
Robert Dyjas <15113729+robdy@users.noreply.github.com>
2021-04-21 20:29:32 +0200

    Add changelog processing

--- CheckModuleData.ps1
+++ CheckModuleData.ps1
@@ -32,6 +32,8 @@
 $dataFolder = 'data'
 $dataCmdletsFolder = Join-Path $dataFolder 'cmdlets'
 $cmdletsFilePath = Join-Path $dataCmdletsFolder 'cmdlets.json'
+$changelogPath = Join-Path $dataFolder 'changelog.json'
+$timeFormatString = '%Y-%m-%d %H:%M:%S'
 
 # ================
 #endregion Variables
@@ -58,15 +60,53 @@ try {
   Write-Error $err
 }
 
+if (Test-Path $changelogPath) {
+  $changelogContent = Get-Content -Path $changelogPath | ConvertFrom-Json
+}
+
 # ================
 #region Process cmdlets
 # ================
-if (-not (Test-Path $dataCmdletsFolder)) {
-  New-Item -ItemType Directory $dataCmdletsFolder | Out-Null
+try {
+  if (-not (Test-Path $dataCmdletsFolder)) {
+    # No directory, create it
+    New-Item -ItemType Directory $dataCmdletsFolder | Out-Null
+  }
+
+  if (Test-Path $cmdletsFilePath) {
+    # Cached file exists, import it
+    $cachedCmdlets = Get-Content -Path $cmdletsFilePath | ConvertFrom-Json
+  }
+
+  $currentCmdlets = Get-Command -Module 'MicrosoftTeams'
+  $addedCmdlets = @($currentCmdlets | Where-Object -FilterScript {$_.Name -notin $cachedCmdlets.Name})
+  $removedCmdlets = @($cachedCmdlets | Where-Object -FilterScript {$_.Name -notin $currentCmdlets.Name})
+  Write-Host "Added cmdlets:   $($addedCmdlets.Count)"
+  Write-Host "Removed cmdlets: $($removedCmdlets.Count)"
+
+  foreach ($addedCmdlet in $addedCmdlets) {
+    $changelogContent = @([pscustomobject]@{
+      Category = "Cmdlet"
+      Object = $addedCmdlet.Name
+      Event = "Add"
+      Timestamp = Get-Date -UFormat $timeFormatString
+    }) + $changelogContent
+  }
+  foreach ($removedCmdlet in $removedCmdlets) {
+    $changelogContent = @([pscustomobject]@{
+      Category = "Cmdlet"
+      Object = $removedCmdlet.Name
+      Event = "Remove"
+      Timestamp = Get-Date -UFormat $timeFormatString
+    }) + $changelogContent
+  }
+
+  $currentCmdlets | Select-Object Name, CommandType | ConvertTo-Csv | ConvertFrom-Csv | ConvertTo-Json -Depth 10 | Out-File $cmdletsFilePath -Force
+} catch {
+  $err = $_
+  Write-Host "Error processing MicrosofTeams cmdlets"
+  Write-Error $err
 }
-$currentCmdlets = Get-Command -Module 'MicrosoftTeams'
-$currentCmdlets.Name
-$currentCmdlets | Select-Object Name, CommandType | ConvertTo-Csv | ConvertFrom-Csv | ConvertTo-Json -Depth 10 | Out-File $cmdletsFilePath -Force
 
 # ================
 #endregion Process cmdlets
@@ -89,6 +129,9 @@ $currentCmdlets | Select-Object Name, CommandType | ConvertTo-Csv | ConvertFrom-
 # ================
 
 # Disconnect Teams
+
+# Export changelog
+$changelogContent | ConvertTo-Json -Depth 3 | Out-File $changelogPath -Force
 
 # ================
 #endregion Processing
